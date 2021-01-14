f839277fcc544294e85374644ba9a4e56723f783
George Knapasjo <george@Georges-MacBook-Pro.local>
2021-01-14 16:27:10 +1100

    readme added/edited

--- src/FormatFunctions.test.js
+++ src/FormatFunctions.test.js
@@ -4,7 +4,7 @@ import Vehicle from './Vehicles/Vehicle'
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Registrations from './Registrations';
-import {FormatDate, RegistrationCalculator, RegistrationStatus} from './FormatFunctions';
+import { FormatDate, RegistrationCalculator, RegistrationStatus } from './FormatFunctions';
 
 //SEPARATE TEST FILE CREATED TO TEST FUNCTION CALLS 
 //AS MOCKING IN THE SAME FILE WAS CAUSING OTHER TESTS TO FAIL
@@ -52,14 +52,14 @@ Object.defineProperty(window, 'location', {
 
 jest.mock('./FormatFunctions.js', () => ({
     ...jest.requireActual('./FormatFunctions.js'),
-    FormatDate:  jest.fn(),
-    RegistrationCalculator:jest.fn(),
+    FormatDate: jest.fn(),
+    RegistrationCalculator: jest.fn(),
     RegistrationStatus: jest.fn()
 }));
 
 describe('Functions are called', () => {
-    describe('<Registraions/>', ()=>{
-        it('Renders', ()=>{
+    describe('<Registraions/>', () => {
+        it('Renders', () => {
             const div = document.createElement('div');
             ReactDOM.render(<Registrations vehicleDetails={vehicleDetails} loading={false} />, div);
         })
@@ -69,34 +69,33 @@ describe('Functions are called', () => {
             expect(FormatDate).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
         });
         it('calls FormatDate with correct date', () => {
-            render(<Registrations vehicleDetails={vehicleDetails} loading={false}/>);
+            render(<Registrations vehicleDetails={vehicleDetails} loading={false} />);
             expect(RegistrationCalculator).toHaveBeenCalled();
             expect(RegistrationCalculator).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
         });
         it('calls FormatDate with correct date', () => {
-            render(<Registrations vehicleDetails={vehicleDetails} loading={false}/>);
+            render(<Registrations vehicleDetails={vehicleDetails} loading={false} />);
             expect(RegistrationStatus).toHaveBeenCalled();
             expect(RegistrationStatus).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
         });
     });
 
-describe('<Vehicle/>', () => {
-    it('calls FormatDate with correct date', () => {
-        render(<Vehicle details={vehicleDetails} />);
-        expect(FormatDate).toHaveBeenCalled();
-        expect(FormatDate).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
+    describe('<Vehicle/>', () => {
+        it('calls FormatDate with correct date', () => {
+            render(<Vehicle details={vehicleDetails} />);
+            expect(FormatDate).toHaveBeenCalled();
+            expect(FormatDate).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
+        });
+        it('calls FormatDate with correct date', () => {
+            render(<Vehicle details={vehicleDetails} />);
+            expect(RegistrationCalculator).toHaveBeenCalled();
+            expect(RegistrationCalculator).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
+        });
+        it('calls FormatDate with correct date', () => {
+            render(<Vehicle details={vehicleDetails} />);
+            expect(RegistrationStatus).toHaveBeenCalled();
+            expect(RegistrationStatus).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
+        });
     });
-    it('calls FormatDate with correct date', () => {
-        render(<Vehicle details={vehicleDetails} />);
-        expect(RegistrationCalculator).toHaveBeenCalled();
-        expect(RegistrationCalculator).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
-    });
-    it('calls FormatDate with correct date', () => {
-        render(<Vehicle details={vehicleDetails} />);
-        expect(RegistrationStatus).toHaveBeenCalled();
-        expect(RegistrationStatus).toHaveBeenLastCalledWith('2021-07-20T23:15:30.000Z');
-    });
-});
-
 });
 
